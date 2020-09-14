513f05c0689c372010ba6817365074afe9361bf1
ViolaCello <ViolaCelloNYC@gmail.com>
2020-09-14 16:43:44 -0400

    get latest Goal updates into array to prepare to view

--- app/controllers/dataviews_controller.rb
+++ app/controllers/dataviews_controller.rb
@@ -1,6 +1,17 @@
 class DataviewsController < ApplicationController
 
 get '/community' do
+    g = Goal.all 
+    last_first = g.reverse
+    @community = []
+    last_first.map do |i|
+        if i.drummer_id 
+            @community.push(i)
+        end
+      #  binding.pry
+    end
+
+
     erb :community
 end
 
