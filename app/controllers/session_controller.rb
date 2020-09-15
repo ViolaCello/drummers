eba52df330a243f835bdf59d94aa9432c9278857
ViolaCello <ViolaCelloNYC@gmail.com>
2020-09-15 10:56:04 -0400

    can create account and redirect to their own profile page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,19 +1,20 @@
 class DrummerController < ApplicationController
 
     get '/join' do
-        if logged_in?
-            redirect '/'
-        else
+        
+        # if logged_in?
+        #     redirect '/'
+        # else
          erb :"/account/join"
-        end
+     #   end
     end
 
     post '/join' do
         @user = Drummer.new(username: params["username"], password: params["password"], hometown: params["hometown"], favorite_drummer: params["favorite_drummer"])
         if @user.save
             session[:user_id] = @user.id
-          #  binding.pry
-            redirect '/profile/#{@user.id}'
+           # binding.pry
+            redirect "/profile/#{@user.id}"
         else
            redirect '/join'
         end
