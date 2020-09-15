a841e6b60e8a3e75b6d8d2b6958c65a106f782a2
ViolaCello <ViolaCelloNYC@gmail.com>
2020-09-15 18:30:30 -0400

    patch - practice/:id/:exercise

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -24,7 +24,27 @@ class PracticesController < ApplicationController
             redirect '/'
         else 
             @update = Lesson.find_by(:exercise => params[:exercise])
+            @goals = @update.goals.where(:drummer_id => current_user.id).last
+            # binding.pry
             erb :"/account/edit_bpm"
         end
     end
+
+    patch '/practice/:id/:exercise' do
+        if !logged_in? || current_user.id != params[:id].to_i
+            redirect '/'
+        else
+            drummer = Drummer.find_by_id(params[:id])
+            lesson = Lesson.find_by(:exercise => params[:exercise])
+           # binding.pry
+            if practice = Goal.create(current: params[:current].to_i, aim: params[:aim].to_i)
+                drummer.goals << practice
+                lesson.goals << practice
+            else  
+                 redirect "/practice/#{current_user.id}"
+            end
+
+       redirect "/profile/#{current_user.id}"
+end
+end
 end
