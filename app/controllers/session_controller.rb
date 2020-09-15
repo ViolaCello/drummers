70d99198f14a5d1b5651f8e7cc5ad5c0cb87e6e3
ViolaCello <ViolaCelloNYC@gmail.com>
2020-09-15 10:30:14 -0400

    post /join

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,22 @@
 class DrummerController < ApplicationController
 
     get '/join' do
-        
+        if logged_in?
+            redirect '/'
+        else
+         erb :"/account/join"
+        end
     end
 
     post '/join' do
-        
+        @user = Drummer.new(username: params["username"], password: params["password"], hometown: params["hometown"], favorite_drummer: params["favorite_drummer"])
+        if @user.save
+            session[:user_id] = @user.id
+          #  binding.pry
+            redirect '/profile/#{@user.id}'
+        else
+           redirect '/join'
+        end
     end
 
     get '/login' do
