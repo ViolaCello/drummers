36b33077216998c98b3995da843e6597c2d48984
ViolaCello <ViolaCelloNYC@gmail.com>
2020-09-24 16:21:21 -0400

    /lessons:id  -- made much more DRY by using ActiveRecord assoication method

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -3,14 +3,17 @@ class LessonController < ApplicationController
     get '/lessons/:id' do
        
         @lesson = Lesson.find_by_id(params[:id].to_i)
-        goal = Goal.where(:lesson_id => params[:id].to_i)
-        drummers = []
-        goal.map do |i| # need to iterate through the goals to remove all the duplicate drummers from the particular Lesson exercises
-            a = i.drummer_id
-            b = goal.where(:drummer_id => a).last
-            drummers.push(b)
-        end
-        @goal = drummers.uniq
+       #  goal = Goal.where(:lesson_id => params[:id].to_i) 
+      # goal = @lesson.goals # replaced with ActiveRecord association method
+       # drummers = []
+       # goal.map do |i| # need to iterate through the goals to remove all the duplicate drummers from the particular Lesson exercises
+            # a = i.drummer_id
+            # b = goal.where(:drummer_id => a).last
+            # drummers.push(b)
+          #  drummers.push(i)
+        #end
+      # made above all more DRY by using ActiveRecord association 
+        @goal = @lesson.drummers.uniq
           erb :lessons
     end
 
